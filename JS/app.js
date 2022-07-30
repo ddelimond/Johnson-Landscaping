74a3f3391316cfc0039307ba4ba3311c8bf06a55
Darren Delimond <darrendelimond@gmail.com>
2022-07-29 21:41:01 -0400

    added updated image modal

--- JS/app.js
+++ JS/app.js
@@ -8,41 +8,36 @@ let clientImages = [
     { img: "/Assets/Past Jobs/House-1-Side.jpeg" },
     { img: "/Assets/Past Jobs/House-1.jpeg" }
 ]
-let clientImage = document.querySelector('.image');
-let numOfClientImages = clientImages.length;
-let indexOfLastClientImage = clientImages.length - 1;
-let lastClientImage = clientImages[clientImages.length - 1];
-let prevButton = document.querySelector('.left-btn');
-let nextButton = document.querySelector('.right-btn');
-let intialIndex = 0;
+
+// let intialIndex = 0;
 let date = document.querySelector('.date');
 let header = document.querySelector('header');
 let scrollLinks = document.querySelectorAll('.scroll-link')
 
 // Button Functionality
 
-let next = function () {
-    if (intialIndex === 0) { intialIndex++; showClient(intialIndex) }
-    else if (intialIndex < indexOfLastClientImage) { intialIndex++; showClient(intialIndex) }
-    else if (intialIndex === indexOfLastClientImage) { intialIndex = 0; showClient(intialIndex) }
-}
-let prev = function () {
-    if (intialIndex === 0) { intialIndex = indexOfLastClientImage; showClient(intialIndex) }
-    else if (intialIndex > 0) { intialIndex--; showClient(intialIndex) }
-}
-nextButton.addEventListener('click', next);
-prevButton.addEventListener('click', prev);
+// let next = function () {
+//     if (intialIndex === 0) { intialIndex++; showClient(intialIndex) }
+//     else if (intialIndex < indexOfLastClientImage) { intialIndex++; showClient(intialIndex) }
+//     else if (intialIndex === indexOfLastClientImage) { intialIndex = 0; showClient(intialIndex) }
+// }
+// let prev = function () {
+//     if (intialIndex === 0) { intialIndex = indexOfLastClientImage; showClient(intialIndex) }
+//     else if (intialIndex > 0) { intialIndex--; showClient(intialIndex) }
+// }
+// nextButton.addEventListener('click', next);
+// prevButton.addEventListener('click', prev);
 
-// Load Client Photos
-window.addEventListener('DOMContentLoaded', function () {
-    showClient(intialIndex);
-})
+// // Load Client Photos
+// window.addEventListener('DOMContentLoaded', function () {
+//     showClient(intialIndex);
+// })
 
 
-function showClient(index) {
-    let imgSource = clientImages[index];
-    clientImage.src = imgSource.img;
-}
+// function showClient(index) {
+//     let imgSource = clientImages[index];
+//     clientImage.src = imgSource.img;
+// }
 
 // Dynamic Copyright Date
 
@@ -109,5 +104,84 @@ scrollLinks.forEach(function (link) {
 
 
 
+// Modal
 
+// get selector and checks to makesure that it is an element
+
+function getElement(selector) {
+    let element = document.querySelector(selector);
+    if (element) { return element }
+    else { throw Error(`Plese check your selector ${selector}, the element does not exist`) }
+}
+
+function Gallery(element) {
+    this.container = element;
+    this.clientSectionImages = [...element.querySelectorAll('.image')];
+    this.modal = getElement('.modal-overlay');
+    this.mainModalImage = getElement('.main-image');
+    this.modalBottom = getElement('.modal-bottom');
+    this.listOfModalImages = this.modal.querySelectorAll('.modal-image');
+    this.closeBtn = getElement('.close-btn');
+    this.prevBtn = getElement('.prev-btn');
+    this.nextBtn = getElement('.next-btn');
+    this.container.addEventListener('click', function (e) {
+        let selectedImage = e.target;
+        this.open(selectedImage, this.clientSectionImages);
+    }.bind(this))
+
+}
+
+Gallery.prototype.open = function (image, list) {
+    this.modal.classList.add('open');
+    this.mainModalImage.src = image.src
+    this.mainModalImage.dataset.id = image.dataset.id
+    this.modalBottom.innerHTML = list.map(function (image) {
+        return `
+<img src="${image.src}" data-id="${image.dataset.id}" class="${image.dataset.id === this.mainModalImage.dataset.id ? "modal-image selected" : "modal-image"}">
+`
+    }.bind(this)).join('')
+
+    this.closeBtn.addEventListener('click', this.close.bind(this))
+    this.prevBtn.addEventListener('click', this.prev.bind(this))
+    this.nextBtn.addEventListener('click', this.next.bind(this))
+    this.modalBottom.addEventListener('click', function (e) {
+        let selected = e.target;
+        this.clickOnModalImage(selected)
+    }.bind(this))
+}
+
+Gallery.prototype.close = function () {
+    this.modal.classList.remove('open');
+    this.closeBtn.removeEventListener('click', this.close.bind(this));
+    this.prevBtn.removeEventListener('click', this.prev.bind(this));
+    this.nextBtn.removeEventListener('click', this.next.bind(this));
+}
+
+Gallery.prototype.next = function () {
+    const selectedImage = document.querySelector('.selected');
+    const next = document.querySelector('.selected').nextElementSibling || this.modalBottom.firstElementChild;
+    selectedImage.classList.remove('selected');
+    next.classList.add('selected');
+    this.mainModalImage.src = next.src;
+}
+Gallery.prototype.prev = function () {
+    const selectedImage = document.querySelector('.selected');
+    const prev = document.querySelector('.selected').previousElementSibling || this.modalBottom.lastElementChild;
+    selectedImage.classList.remove('selected');
+    prev.classList.add('selected');
+    this.mainModalImage.src = prev.src;
+}
+
+Gallery.prototype.clickOnModalImage = function (image) {
+    const selectedImage = image;
+    const prevSelectedImage = document.querySelector('.selected');
+    prevSelectedImage.classList.remove('selected');
+    selectedImage.classList.add('selected');
+    this.mainModalImage.src = selectedImage.src;
+}
+
+
+
+
+let clientImageContainer = new Gallery(getElement('.client-container'))
 
